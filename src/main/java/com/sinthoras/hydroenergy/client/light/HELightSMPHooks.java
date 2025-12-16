8bec0e7c650843a16ad8a7b8ffdbe6ce551cd3fb
chochem <40274384+chochem@users.noreply.github.com>
2025-12-16 17:26:37 +0000

    Double, double toil and trouble (#37)

--- src/main/java/com/sinthoras/hydroenergy/client/light/HELightSMPHooks.java
+++ src/main/java/com/sinthoras/hydroenergy/client/light/HELightSMPHooks.java
@@ -4,17 +4,19 @@ import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
+import cpw.mods.fml.relauncher.FMLLaunchHandler;
+
 public class HELightSMPHooks {
 
     // this method filters @Side.SERVER out and only passes client side calls on
     public static void onSetBlock(World world, int blockX, int blockY, int blockZ, Block block, Block oldBlock) {
-        if (world.isRemote && oldBlock != null) {
+        if (world.isRemote && FMLLaunchHandler.side().isClient() && oldBlock != null) {
             HELightManager.onSetBlock(blockX, blockY, blockZ, block, oldBlock);
         }
     }
 
     public static void onLightUpdate(Chunk chunk, int blockX, int blockY, int blockZ) {
-        if (chunk.worldObj.isRemote) {
+        if (chunk.worldObj.isRemote && FMLLaunchHandler.side().isClient()) {
             HELightManager.onLightUpdate(chunk, blockX, blockY, blockZ);
         }
     }
