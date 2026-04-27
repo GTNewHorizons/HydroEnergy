0af3f5e991efe50e5766a89a9191cd5a07451d22
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-04-27 22:18:35 +0200

    fix world client leak

--- src/main/java/com/sinthoras/hydroenergy/mixins/late/GT_PollutionRendererMixin.java
+++ src/main/java/com/sinthoras/hydroenergy/mixins/late/GT_PollutionRendererMixin.java
@@ -8,29 +8,12 @@ import net.minecraftforge.client.event.EntityViewRenderEvent;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import gregtech.common.pollution.PollutionRenderer;
 
 @Mixin(value = PollutionRenderer.class)
 public class GT_PollutionRendererMixin {
-
-    private EntityViewRenderEvent.FogColors event;
-
-    // Grab eye position for subsequent getMaterial call
-    @Inject(
-            method = "manipulateColor",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/block/Block;getMaterial()Lnet/minecraft/block/material/Material;",
-                    shift = At.Shift.BEFORE,
-                    ordinal = 0),
-            require = 1)
-    private void beforeMaterialIsWaterCheck(EntityViewRenderEvent.FogColors event, CallbackInfo callbackInfo) {
-        this.event = event;
-    }
 
     // Redirect getMaterial to check for custom water
     @Redirect(
@@ -40,7 +23,7 @@ public class GT_PollutionRendererMixin {
                     target = "Lnet/minecraft/block/Block;getMaterial()Lnet/minecraft/block/material/Material;",
                     ordinal = 0),
             require = 1)
-    private Material redirectMaterialIsWaterCheck(Block instance) {
+    private Material redirectMaterialIsWaterCheck(Block instance, EntityViewRenderEvent.FogColors event) {
         return getMaterialWrapper(instance, event.entity.posY + event.entity.getEyeHeight());
     }
 }
